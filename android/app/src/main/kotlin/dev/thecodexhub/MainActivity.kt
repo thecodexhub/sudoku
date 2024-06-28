32197d021aa246a84bd343554b85067ca9db1227
Sandip <sandipramanik07@gmail.com>
2024-06-28 19:23:38 +0530

    initial commit ✨

---
+++ android/app/src/main/kotlin/dev/thecodexhub/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.thecodexhub.sudoku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:sudoku/app/app.dart';
+import 'package:sudoku/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
