226787d5eb9fa65a3db94ddc2cf2f394d1dfc0c2
Sandip <sandipramanik07@gmail.com>
2024-07-17 02:16:46 +0530

    feat: set up environment variables

---
+++ lib/env/env.dart
@@ -0,0 +1,9 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied(path: '.env', obfuscate: true)
+abstract class Env {
+  @EnviedField(varName: 'API_BASE_URL')
+  static final String apiBaseUrl = _Env.apiBaseUrl;
+}
