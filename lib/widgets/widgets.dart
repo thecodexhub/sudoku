e2b824561b84af8e79edc11af1e6e5c95b076bea
Sandip Pramanik <thecodexhubofficial@gmail.com>
2024-08-09 10:36:28 +0530

    feat: add copyright, license in footer of home page (#12)
    * feat: add copyright, license in footer of home page
    * feat: minor ui updates

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'sudoku_background.dart';
 export 'sudoku_elevated_button.dart';
 export 'sudoku_failure_dialog.dart';
+export 'sudoku_footer.dart';
 export 'sudoku_icon.dart';
 export 'sudoku_loading_dialog.dart';
 export 'sudoku_text_button.dart';
